ff4abb24599842d219fb6e6bd23f165952cddbaf
chanoir2303 <chanoir2303@protonmail.com>
2022-02-13 15:43:09 +0100

    Add files via upload

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Kévin Selen on 12/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Landmarks/Landmarks/Views/LandmarkRow.swift
@@ -0,0 +1,24 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by Kévin Selen on 13/02/2022.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+    
+    var body: some View {
+        HStack {
+            Text(landmark.name)
+        }
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkRow(landmark: landmarks[0])
+    }
+}
